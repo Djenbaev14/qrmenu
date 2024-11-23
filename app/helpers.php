21f3e2fc7b1e826dd269f7c6610a41c918491252
Djenbaev14 <89042687+Djenbaev14@users.noreply.github.com>
2024-11-23 12:47:24 +0500

    fefe

--- app/helpers.php
+++ app/helpers.php
@@ -3,7 +3,7 @@ use Stichoza\GoogleTranslate\GoogleTranslate;
 use App\Models\Order;
 
 function is_answered(){
-  $company_id=auth()->user()->company->first()->id;
+  $company_id=auth()->user()->company->id;
   return  Order::where('company_id', $company_id)->where("deleted_at",null)->where("is_answered",0)->count();
 }
 function translate($text, $targetLanguage = 'uz') {
