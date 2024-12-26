2af9a819d20bb540b18943902c14deff4099f06b
Djenbaev14 <89042687+Djenbaev14@users.noreply.github.com>
2024-12-26 17:06:52 +0500

    e31e13

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,10 +3,5 @@
 
 @section('content')
   <div class="content-body">
-    <form action="{{route('auth.register')}}" method="POST">
-      @csrf
-      <input type="text" name="phone" id="">
-      <button>submit</button>
-    </form>
   </div>
 @endsection
